447b377d0dfdf8c8e61311360ac3a2a7e9bd0049
Isaac Springer <idspringer@onyxhat.local>
2016-12-19 08:48:54 -0600

    Initial commit

---
+++ easel.drvier/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+###Functions
+function Parse-ChocoParams() {
+    $match_pattern = "(?<key>(\w+))\s*=\s*(?<value>([`"'])?([\w- _\\:\.]+)([`"'])?)"
+
+    if ($packageParameters -match $match_pattern) {
+        $results = $packageParameters | Select-String $match_pattern -AllMatches
+        $results.matches | % {
+            Set-Variable -Name $_.Groups['key'].Value.Trim() -Value $_.Groups['value'].Value.Trim() -Scope Global
+        }
+    }
+}
+
+###Preflight
+$packageName  = 'easel.drvier' # arbitrary name for the package, used in messages
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://s3.amazonaws.com/easel-prod/paperclip/sender_version_windows_installers/23/original/EaselDriver-0.3.2.zip?1478118532' # download url, HTTPS preferred
+$checkSum     = 'f978a72afab10ac1d347fc00648b4a70'
+$checkSumType = 'MD5'
+
+###Install
+Install-ChocolateyZipPackage -PackageName "easel.driver" -Url $url -UnzipLocation $toolsDir -Checksum $checkSum -ChecksumType $checkSumType
---
+++ easel.drvier/tools/chocolateyuninstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'easel.drvier'
+$softwareName = 'easel.drvier*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = "$($_.UninstallString)"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | % {Write-Warning "- $_.DisplayName"}
+}
